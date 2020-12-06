773b41f29ade9d9d035d90973df187c1dbc5a0ed
webmaster442 <ruzsinszki.gabor@gmail.com>
2020-12-06 18:26:29 +0100

    Fixed build warnings

--- NMaier.Windows.Forms/Properties/AssemblyInfo.cs
+++ NMaier.Windows.Forms/Properties/AssemblyInfo.cs
@@ -2,11 +2,6 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("381b29a0-7e27-49fa-ac74-f740c2da1f00")]
-#endif
