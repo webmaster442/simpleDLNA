5ba4a3e6e19f975b65d92ae58306cd4ae7c34714
webmaster442 <ruzsinszki.gabor@gmail.com>
2020-12-06 19:18:32 +0100

    git commit -m "Removed Windows forms GUI project"

--- NMaier.Windows.Forms/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
-[assembly: Guid("381b29a0-7e27-49fa-ac74-f740c2da1f00")]
